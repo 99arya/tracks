c3589319593a67d11834064a8d3e7f06fd2645d2
Sumit Arya <sumit.arya@thoughts2binary.com>
2020-11-23 00:51:01 +0530

    auth

--- track-server/src/index.js
+++ track-server/src/index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const authRoutes = require("./routes/authRoutes");
+const requireAuth = require("./middlewares/requireAuth");
 
 const app = express();
 app.use(bodyParser.json());
@@ -24,8 +25,8 @@ mongoose.connection.on("error", (err) => {
   console.log("Error connecting TO DB", err);
 });
 
-app.get("/", (req, res) => {
-  res.send("Hi there!!");
+app.get("/", requireAuth, (req, res) => {
+  res.send(req.user);
 });
 
 app.listen("3000", () => {
