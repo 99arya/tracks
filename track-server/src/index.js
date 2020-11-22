d36cb5cff55bdee2f9b5881e286be87cb51be2a2
Sumit Arya <sumit.arya@thoughts2binary.com>
2020-11-23 00:29:38 +0530

    first commit

---
+++ track-server/src/index.js
@@ -0,0 +1,33 @@
+require("./models/User");
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const authRoutes = require("./routes/authRoutes");
+
+const app = express();
+app.use(bodyParser.json());
+app.use(authRoutes);
+
+const mongoUri = "mongodb+srv://admin:passwordpassword@cluster0.cg5dt.mongodb.net/Cluster0?retryWrites=true&w=majority";
+
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("CONNECTED TO DB");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log("Error connecting TO DB", err);
+});
+
+app.get("/", (req, res) => {
+  res.send("Hi there!!");
+});
+
+app.listen("3000", () => {
+  console.log("SERVER RUNNING");
+});
